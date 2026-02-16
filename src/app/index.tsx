f7149d13398d2c2b57aabb2b8aabc69c49d29dad
Micael-Macedo <micaelm2009@hotmail.com>
2026-02-16 16:15:21 -0300

    feat: add aula criacao rota in progress

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,9 +10,14 @@ export default function Index() {
             <Button 
             title="Nova Meta" 
             onPress={() => router.navigate("/target")} />
+
             <Button 
             title="Transação" 
             onPress={() => router.navigate("/transaction/132")} />
+
+            <Button 
+            title="Progresso" 
+            onPress={() => router.navigate("/in-progress/132")} />
 
         </View>
     )
--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -3,6 +3,7 @@ import { Button, Text, View } from "react-native";
 
 export default function Transaction() {
     const params = useLocalSearchParams<{id: string}>()
+
     return(
         <View style={{ flex: 1, justifyContent:"center" }}>
             <Text>ID: {params.id}</Text>
